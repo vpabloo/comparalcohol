6d5802ff9bd7da0d6806966e8fa6bcda3e691ea4
= <=>
2022-08-22 14:05:20 -0500

    lo mismo pero en js

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function concatena_cerveza() {
+    var marca = document.getElementById('marca').value;
+    var presentacion = document.getElementById('presentacion').value;
+    var mililitros = document.getElementById('mililitros').value;
+    var precio = document.getElementById('precio').value;
+
+    if (mililitros === '' || precio === '') {
+        alert("Ingrese almenos mililitros y precio")
+    } else {
+        precio_mililitros = (precio / presentacion) / mililitros
+        cerveza = presentacion + '\u00A0Chela(s)\u00A0' + marca + '\u00A0de\u00A0' + mililitros + 'ml\u00A0c/u.\u00A0Cuesta\u00A0$' + precio + '\u00A0quedando\u00A0' + "$" + parseFloat(precio_mililitros).toFixed(3) + '\u00A0' + "por mililitro."
+
+        // Creamos un nuevo elemento LI
+        var li = document.createElement('li');
+        // Añadimos el valor introducido al nuevo elemento
+        li.innerHTML = cerveza;
+        // Añadimos el elemento LI a la lista UL
+        var ul = document.getElementById('lista');
+        ul.appendChild(li);
+        // Vaciamos la caja de texto
+        document.getElementById('marca').value = "";
+        document.getElementById('presentacion').value = "1";
+        document.getElementById('mililitros').value = "";
+        document.getElementById('precio').value = "";
+    }
+}
